908b2052b2916d68f986b156554ac229615c32b7
YareliHH <yhh122004@gmail.com>
2024-10-22 03:18:34 -0600

    add fother

--- src/Componentes/Compartidos/Footer.jsx
+++ src/Componentes/Compartidos/Footer.jsx
@@ -1,50 +1,44 @@
 import React from 'react';
-import { Container, Grid, Typography, IconButton } from '@mui/material';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
-    <footer style={{ backgroundColor: '#00bcd4', color: '#ffffff', padding: '20px', width: '100%', bottom: 0 }}>
-      <Container maxWidth="lg">
-        <Grid container spacing={4} alignItems="center">
-          
-          {/* Atención al Cliente */}
-          <Grid item xs={12} md={6}>
-            <Typography variant="h6" gutterBottom>
-              Atención al Cliente
-            </Typography>
-            <Typography variant="body1">
-              Teléfono: <a href="tel:+1234567890" style={{ color: '#ffffff' }}>7711730977</a><br />
-              Correo: <a href="mailto:atencion@gislive.com" style={{ color: '#ffffff' }}>Jehiely_24@hotmail.com</a>
-            </Typography>
-          </Grid>
-
-          {/* Redes Sociales */}
-          <Grid item xs={12} md={6} textAlign={{ xs: 'center', md: 'right' }}>
-            <Typography variant="h6" gutterBottom>
-              Síguenos en nuestras redes sociales
-            </Typography>
-            <div>
-              <IconButton href="https://www.facebook.com" target="_blank" style={{ color: '#ffffff' }}>
-                <FontAwesomeIcon icon={faFacebook} size="2x" />
-              </IconButton>
-              <IconButton href="https://www.instagram.com" target="_blank" style={{ color: '#ffffff' }}>
-                <FontAwesomeIcon icon={faInstagram} size="2x" />
-              </IconButton>
-              <IconButton href="mailto:atencion@gislive.com" style={{ color: '#ffffff' }}>
-                <FontAwesomeIcon icon={faEnvelope} size="2x" />
-              </IconButton>
-            </div>
-          </Grid>
-        </Grid>
-
-        {/* Copyright */}
-        <Typography variant="body2" textAlign="center" style={{ marginTop: '20px' }}>
-          © 2024 Gislive. Todos los derechos reservados.
-        </Typography>
-      </Container>
+    <footer style={{ backgroundColor: '#00bcd4', color: '#ffffff', padding: '20px', textAlign: 'center', position: 'fixed', width: '100%', bottom: 0 }}>
+      
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexWrap: 'wrap', maxWidth: '800px', margin: '0 auto' }}>
+        
+        {/* Atención al Cliente */}
+        <div>
+          <h4>Atención al Cliente</h4>
+          <p>
+            Teléfono: <a href="tel:+1234567890" style={{ color: '#ffffff' }}>7711730977</a><br />
+            Correo: <a href="mailto:atencion@gislive.com" style={{ color: '#ffffff' }}>Jehiely_24@hotmail.com</a>
+          </p>
+        </div>
+        
+        {/* Redes Sociales */}
+        <div>
+          <h4>Síguenos en nuestras redes sociales</h4>
+          <div>
+            <Link to={{ pathname: "https://www.facebook.com" }} target="_blank" style={{ margin: '0 10px', color: '#ffffff' }}>
+              <FontAwesomeIcon icon={faFacebook} size="2x" />
+            </Link>
+            <Link to={{ pathname: "https://www.instagram.com" }} target="_blank" style={{ margin: '0 10px', color: '#ffffff' }}>
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </Link>
+            <Link to="mailto:atencion@gislive.com" style={{ margin: '0 10px', color: '#ffffff' }}>
+              <FontAwesomeIcon icon={faEnvelope} size="2x" />
+            </Link>
+          </div>
+        </div>
+        
+      </div>
+      
+      <p style={{ marginTop: '10px' }}>© 2024 Gislive. Todos los derechos reservados.</p>
+      
     </footer>
   );
 }
