e18e6c8e84e8e20d911d37cd93a16238bd67135b
YareliHH <yhh122004@gmail.com>
2024-10-21 22:46:22 -0600

    logo

--- src/Componentes/Compartidos/barraNav.jsx
+++ src/Componentes/Compartidos/barraNav.jsx
@@ -7,7 +7,7 @@ import InfoIcon from '@mui/icons-material/Info';
 import LoginIcon from '@mui/icons-material/Login';
 import { useNavigate } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import logo from '../imagenes/logoGL.jpg';
+import logo from '../imagenes/LogoGL.jpg';
 
 // Creación del tema personalizado
 const theme = createTheme({
