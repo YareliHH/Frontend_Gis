b78801261365a5f38f6c8051c4c5ee1282c8af3a
YareliHH <yhh122004@gmail.com>
2024-11-25 21:24:18 -0600

    perfil de empresa

--- src/Componentes/Administrativo/PerfilEmpresa.jsx
+++ src/Componentes/Administrativo/PerfilEmpresa.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 const PerfilEmpresa = () => {
   const [perfil, setPerfil] = useState({
     nombre_empresa: "",
-    logo: null,
+    logo: "",
     direccion: "",
     telefono: "",
     correo_electronico: "",
@@ -351,7 +351,12 @@ const PerfilEmpresa = () => {
                 <TableCell>{item.titulo_pagina}</TableCell>
                 <TableCell>{item.descripcion}</TableCell>
                 <TableCell>
-                  <Avatar src={item.logo} alt="Logo" sx={{ width: 50, height: 50 }} />
+                <img
+                  src="https://res.cloudinary.com/dytchcrpf/image/upload/v1732008306/logos_empresas/kqxv4lwtsbc47fvk5zny.jpg" // Imagen genérica en caso de error
+                  alt={`${item.nombre_empresa} Logo`}
+                  style={{ width: 50, height: 50, objectFit: "cover", borderRadius: "8px" }}
+                />
+
                 </TableCell>
               </TableRow>
             ))}
